8d087be6fb393fbb9f83fecd867a59a47d29b2d4
root <yluo3421@gmail.com>
2022-04-27 11:16:58 -0400

    commit tree cbea77f3a2d6dead2574e9c114236f0ba215c17e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array, str) {
+    return array.filter((name) => name.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(array, str) {
+    return array.filter((name) => name.charAt(0) === str.charAt(0));
+
+}
+
+function matchName(array, str) {
+    return array.filter((obj) => obj.name.toLowerCase() === str.toLowerCase());
+}
